49175db7b259c4afc5ca88e416bf3e271fda8ca3
MatthiasSaliba <146810236+MatthiasSaliba@users.noreply.github.com>
2024-05-08 09:43:06 +0200

    Started working on the main menu. Will do button implementation

---
+++ mainmenu/mainmenu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+#Get button references
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _button1_pressed():
+	#TODO: LOAD Match and Learn
+	pass
+
+func _button2_pressed():
+	#TODO: LOAD 3 Pics 1 Word
+	pass
+
+func _button3_pressed():
+	#TODO: LOAD Find the Word
+	pass
+	
+	
